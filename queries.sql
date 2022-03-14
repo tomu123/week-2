ab8568789d448fe7374e3ff0f4a479e1e966bb1a
Tomu Komatsu <tomu5642514@gmail.com>
2022-03-14 18:48:37 -0500

    Fix query misunderstanding

--- queries.sql
+++ queries.sql
@@ -13,16 +13,17 @@ select p.id,p.name,SUM(ol.quantity) as sales,SUM(ol.total) as profits from "Prod
 -- Update a Product price by 1.5
 update "Products" SET price = price * 1.5 where id = 19;
 -- Select All the customers who bought an X product
-select c.* from "Customers" as c join "Orders" as o on o.customer_id = c.id join "Order Lines" as ol on o.id = ol.order_id WHERE ol.product_id = 16;
+select DISTINCT c.* from "Customers" as c join "Orders" as o on o.customer_id = c.id join "Order Lines" as ol on o.id = ol.order_id WHERE ol.product_id = 16;
 -- Select All orders between dates X and Y
 select o.* from "Orders" as o where o.date BETWEEN '2022-09-27' AND '2022-11-29';
 -- Select All products with price greater than 4.5
 select p.* from "Products" as p where p.price > 4.5;
 -- Select All the products a Customer has bought
-select DISTINCT p.* from "Products" as p join "Order Lines" as ol on ol.product_id = p.id;
+select DISTINCT o.customer_id as customer,p.* from "Products" as p join "Order Lines" as ol on ol.product_id = p.id join "Orders" as o on o.id = ol.order_id where o.customer_id = 1;
 -- Select the total amount of products a X customer bought between 2 dates
-select c.*,SUM(ol.quantity) as "amount of products" from "Order Lines" as ol join "Orders" as o on o.id = ol.order_id join "Customers" as c on c.id = o.customer_id where (o.date BETWEEN '2022-09-27' AND '2022-11-29') AND c.id = 2 GROUP BY c.id;
+select c.*,SUM(ol.quantity) as "amount of products" from "Order Lines" as ol join "Orders" as o on o.id = ol.order_id join "Customers" as c on c.id = o.customer_id where (o.date BETWEEN '2022-09-27' AND '2022-11-29') AND c.id = 1 GROUP BY c.id;
 -- Select what is the most purchased product
 select p.*,SUM(ol.quantity) as sales from "Order Lines" as ol join "Products" as p on p.id = ol.product_id GROUP BY p.id ORDER BY sales DESC LIMIT 1;
 -- Delete an order
 delete from "Orders" where id = (select id from "Orders" LIMIT 1);
+
--- seed.sql
+++ seed.sql
@@ -1,128 +1,124 @@
 INSERT INTO public."Customers"(first_name,last_name,address,phone)
-VALUES('Cordell','Boyle','Apt. 860 50966 Sonia Drives, Larrystad, LA 37236-0658','+1-868 677.905.3679');
+VALUES('Chan','Dietrich','493 Hilario Walks, North Henry, RI 46222','+32 450-638-1361');
 INSERT INTO public."Customers"(first_name,last_name,address,phone)
-VALUES('Michiko','Larkin','Apt. 685 56952 Tambra Motorway, Cronafort, RI 22540','+687 1-597-146-3605');
+VALUES('Deeann','Borer','Apt. 307 5326 Geoffrey Manors, New Shanellborough, OR 80729-8847','+679 1-335-471-4813');
 INSERT INTO public."Customers"(first_name,last_name,address,phone)
-VALUES('Derick','Schroeder','Apt. 647 16772 Marcel Stravenue, Hankside, OK 95392','+213 188.973.0290');
+VALUES('Travis','Kuhlman','85048 Willie Pike, West Markus, IL 09763','+670 1-305-635-4182');
 INSERT INTO public."Customers"(first_name,last_name,address,phone)
-VALUES('Rodger','Willms','Suite 277 97961 Cristopher Harbors, North Craigborough, TX 80953','+676 684-302-9612');
+VALUES('Lavern','Schaefer','606 Shannon Cliff, Paulitaton, MN 93891','+257 1-701-720-1165');
 INSERT INTO public."Customers"(first_name,last_name,address,phone)
-VALUES('Hyon','Reilly','56092 Kozey Wall, South Fredrick, WA 34705-5370','+691 258.077.6029');
+VALUES('Lorraine','White','Apt. 684 15753 Monty Inlet, Lake Shandaland, TN 65299-3116','+34 389-829-8636');
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000HB4DU0','Synergistic Wool Computer','Et repellat eum tenetur.',47.9,94);
+VALUES('B000BHP3T0','Heavy Duty Copper Gloves','Et quam explicabo reiciendis.',18.7,51);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000JQ0JNS','Durable Cotton Car','Magni molestiae omnis consequuntur.',72.5,45);
+VALUES('B000COF89C','Small Leather Computer','Facere expedita accusamus et.',73.1,75);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000I6VJ7O','Ergonomic Marble Shoes','Itaque optio ducimus quidem.',28.0,84);
+VALUES('B000A1AQWQ','Durable Cotton Pants','In fuga quia similique.',2.6,60);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B0000DE0T8','Fantastic Iron Clock','Consequatur consectetur inventore voluptas.',22.9,60);
+VALUES('B000A2SP0K','Synergistic Iron Knife','Cupiditate error odio ad.',95.1,57);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000BJ0Z50','Small Concrete Keyboard','Omnis tempore iste rerum.',5.5,82);
+VALUES('B000BJ20Y4','Ergonomic Copper Keyboard','Neque expedita ratione ea.',13.2,51);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000LCZCRS','Intelligent Steel Chair','Illo officia odit deserunt.',6.8,53);
+VALUES('B000BJ5JHE','Rustic Wooden Gloves','Enim cum unde impedit.',24.0,56);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B0002F03NW','Synergistic Plastic Bag','Vitae aperiam nemo eaque.',59.0,59);
+VALUES('B0000DGTOF','Mediocre Marble Bottle','Autem quia rerum qui.',40.1,49);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000A3PVZ6','Awesome Steel Pants','Dolores dolorem a quos.',7.5,78);
+VALUES('B0009R9L7W','Heavy Duty Bronze Bench','Officiis distinctio eos delectus.',12.8,77);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000A6LQZ2','Enormous Marble Knife','Et cumque aut porro.',10.6,41);
+VALUES('B00067668W','Practical Silk Gloves','Est consequatur autem quae.',92.9,64);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000HB4DU0','Fantastic Rubber Computer','Minima cumque atque ab.',48.0,94);
+VALUES('B0000DYZW5','Sleek Cotton Bag','Tempore voluptatibus praesentium repellat.',34.7,71);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000A3LC18','Aerodynamic Silk Bench','Dolorum soluta rerum quia.',43.2,43);
+VALUES('B0000DGWB4','Mediocre Granite Watch','Commodi vel iusto illum.',10.0,69);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000NI7RW8','Synergistic Wool Bench','Laborum eveniet sed quas.',80.3,91);
+VALUES('B000NMKCKI','Gorgeous Concrete Bottle','Veritatis et quisquam tempora.',1.6,72);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B0002SZELK','Enormous Wooden Clock','Ipsum nobis possimus assumenda.',6.9,63);
+VALUES('B000A6WENA','Synergistic Concrete Keyboard','Mollitia reiciendis ut ipsum.',26.7,92);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B00067FMXM','Aerodynamic Granite Computer','Itaque voluptas dolor expedita.',99.7,97);
+VALUES('B00067PA2U','Lightweight Wool Bottle','Suscipit veniam autem rerum.',80.2,54);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000B7CBWM','Practical Silk Hat','Nihil qui tenetur ullam.',6.5,39);
+VALUES('B000ML8E7I','Aerodynamic Iron Shirt','Dolores vel facilis eos.',79.0,49);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B00067FMXM','Enormous Rubber Clock','Dolore recusandae asperiores rerum.',6.9,71);
+VALUES('B000JR91YA','Sleek Wooden Shoes','Repudiandae ut sapiente ut.',15.2,89);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000Q72CSA','Ergonomic Rubber Keyboard','Earum impedit facilis nihil.',13.3,35);
+VALUES('B000CR7COS','Ergonomic Paper Table','In debitis voluptatum consequatur.',8.1,51);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B0002SUQUY','Lightweight Paper Clock','Deserunt incidunt nobis sunt.',40.6,92);
+VALUES('B000B5MVJM','Mediocre Paper Car','Cumque vitae maiores voluptatem.',75.9,95);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B000A3K6TM','Practical Copper Bag','Ex deleniti ipsum officia.',79.2,95);
+VALUES('B0009QMECC','Heavy Duty Cotton Gloves','Id sit magnam esse.',77.9,45);
 INSERT INTO public."Products"(sku,name,description,price,stock)
-VALUES('B0000DHVM5','Rustic Granite Watch','Temporibus eos fugit ut.',24.6,61);
+VALUES('B00066USX2','Durable Copper Hat','Sint at rerum amet.',88.7,90);
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-09-28',2,'cancelled');
+VALUES('2022-03-14',1,'pending');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 1,3,3,price FROM public."Products" WHERE id = 3;
+SELECT 1,9,4,price FROM public."Products" WHERE id = 9;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 1,3,4,price FROM public."Products" WHERE id = 3;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 1,17,3,price FROM public."Products" WHERE id = 17;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 1,19,3,price FROM public."Products" WHERE id = 19;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-11-28',1,'cancelled');
+VALUES('2022-03-05',3,'completed');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 2,2,5,price FROM public."Products" WHERE id = 2;
+SELECT 2,7,5,price FROM public."Products" WHERE id = 7;
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 2,3,5,price FROM public."Products" WHERE id = 3;
+SELECT 2,11,4,price FROM public."Products" WHERE id = 11;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 2,18,3,price FROM public."Products" WHERE id = 18;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-02-03',3,'cancelled');
+VALUES('2022-11-26',1,'pending');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 3,11,1,price FROM public."Products" WHERE id = 11;
+SELECT 3,4,2,price FROM public."Products" WHERE id = 4;
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 3,10,3,price FROM public."Products" WHERE id = 10;
+SELECT 3,5,2,price FROM public."Products" WHERE id = 5;
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 3,19,5,price FROM public."Products" WHERE id = 19;
+SELECT 3,6,3,price FROM public."Products" WHERE id = 6;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 3,19,3,price FROM public."Products" WHERE id = 19;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 3,20,2,price FROM public."Products" WHERE id = 20;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-11-11',2,'completed');
+VALUES('2022-10-04',3,'pending');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 4,16,1,price FROM public."Products" WHERE id = 16;
+SELECT 4,4,1,price FROM public."Products" WHERE id = 4;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 4,19,3,price FROM public."Products" WHERE id = 19;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-03-12',2,'pending');
+VALUES('2022-12-31',5,'cancelled');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 5,2,2,price FROM public."Products" WHERE id = 2;
+SELECT 5,12,3,price FROM public."Products" WHERE id = 12;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 5,17,5,price FROM public."Products" WHERE id = 17;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 5,15,1,price FROM public."Products" WHERE id = 15;
+INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
+SELECT 5,20,2,price FROM public."Products" WHERE id = 20;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-09-07',4,'pending');
+VALUES('2022-03-12',5,'cancelled');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 6,18,1,price FROM public."Products" WHERE id = 18;
+SELECT 6,9,1,price FROM public."Products" WHERE id = 9;
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 6,8,2,price FROM public."Products" WHERE id = 8;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 6,14,2,price FROM public."Products" WHERE id = 14;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 6,4,5,price FROM public."Products" WHERE id = 4;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 6,9,3,price FROM public."Products" WHERE id = 9;
+SELECT 6,13,5,price FROM public."Products" WHERE id = 13;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-07-14',4,'cancelled');
+VALUES('2022-10-01',1,'cancelled');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 7,1,5,price FROM public."Products" WHERE id = 1;
+SELECT 7,18,5,price FROM public."Products" WHERE id = 18;
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 7,18,1,price FROM public."Products" WHERE id = 18;
+SELECT 7,7,3,price FROM public."Products" WHERE id = 7;
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 7,17,5,price FROM public."Products" WHERE id = 17;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 7,19,2,price FROM public."Products" WHERE id = 19;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 7,16,2,price FROM public."Products" WHERE id = 16;
+SELECT 7,15,1,price FROM public."Products" WHERE id = 15;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-01-03',1,'pending');
+VALUES('2022-08-28',5,'completed');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 8,20,4,price FROM public."Products" WHERE id = 20;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 8,3,3,price FROM public."Products" WHERE id = 3;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 8,17,3,price FROM public."Products" WHERE id = 17;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 8,5,2,price FROM public."Products" WHERE id = 5;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 8,15,3,price FROM public."Products" WHERE id = 15;
+SELECT 8,13,5,price FROM public."Products" WHERE id = 13;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-01-04',5,'cancelled');
+VALUES('2022-02-03',2,'pending');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
 SELECT 9,16,4,price FROM public."Products" WHERE id = 16;
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 9,20,4,price FROM public."Products" WHERE id = 20;
+SELECT 9,19,2,price FROM public."Products" WHERE id = 19;
 INSERT INTO public."Orders"(date,customer_id,status)
-VALUES('2022-05-04',4,'pending');
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 10,6,1,price FROM public."Products" WHERE id = 6;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 10,3,1,price FROM public."Products" WHERE id = 3;
-INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
-SELECT 10,13,3,price FROM public."Products" WHERE id = 13;
+VALUES('2022-07-12',3,'completed');
 INSERT INTO public."Order Lines"(order_id,product_id,quantity,price)
 SELECT 10,12,1,price FROM public."Products" WHERE id = 12;
